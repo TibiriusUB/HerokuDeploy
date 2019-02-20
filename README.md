f5cc7297615e4ac79c73e3ccc326aee8957fcd82
Tibirius <tibirius@gmail.com>
2019-02-20 16:35:11 -0500

    Friend_Finder

---
+++ README.md
@@ -0,0 +1,15 @@
+# Friend Finder
+
+### Unit 13: Node & Express 
+
+![Main page](image.png "FF Home Page")
+
+Friend Finder is a basic match making service web app, that finds a match based on answer value similarity. A server is generated with Node-express, and routes to the relevent resources are created. Relevent paths are made with the PATH module.
+
+![Survey page](image(1).png "FF Survey Page")
+
+Users can take the survey, and thier match gets diplayed via a modal popup. User data then gets added to the mix for further potential matches!
+
+![Survey Result](image(2).png "FF Match Made!")
+
+Enjoy! :)
---
+++ app/data/friends.js
@@ -0,0 +1,191 @@
+var clientList = [
+
+    {
+        "name": "Ahmed",
+        "photo": "https://images.pexels.com/photos/1275311/pexels-photo-1275311.jpeg?cs=srgb&dl=beard-bearded-black-leather-jacket-1275311.jpg&fm=jpg",
+        "scores": [
+            "4",
+            "3",
+            "3",
+            "2",
+            "4",
+            "3",
+            "1",
+            "5",
+            "5",
+            "3"
+        ]
+    },
+
+    {
+        "name": "Bethany",
+        "photo": "https://images.unsplash.com/photo-1550599092-2a050d84e9f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1266&q=80",
+        "scores": [
+            "4",
+            "5",
+            "3",
+            "4",
+            "4",
+            "5",
+            "1",
+            "3",
+            "4",
+            "5"
+        ]
+    },
+
+    {
+        "name": "Cheryl",
+        "photo": "https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?cs=srgb&dl=beautiful-beauty-blurred-background-1239291.jpg&fm=jpg",
+        "scores": [
+            "3",
+            "3",
+            "4",
+            "3",
+            "3",
+            "4",
+            "3",
+            "4",
+            "5",
+            "3"
+        ]
+    },
+
+    {
+        "name": "Donovan",
+        "photo": "https://images.pexels.com/photos/1757108/pexels-photo-1757108.jpeg?cs=srgb&dl=beard-bed-bedroom-1757108.jpg&fm=jpg",
+        "scores": [
+            "5",
+            "3",
+            "3",
+            "1",
+            "1",
+            "3",
+            "3",
+            "5",
+            "5",
+            "4"
+        ]
+    },
+
+    {
+        "name": "Edmund",
+        "photo": "https://images.pexels.com/photos/769772/pexels-photo-769772.jpeg?cs=srgb&dl=boy-casual-cool-769772.jpg&fm=jpg",
+        "scores": [
+            "2",
+            "5",
+            "1",
+            "5",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "5"
+        ]
+    },
+
+    {
+        "name": "Francio",
+        "photo": "https://images.pexels.com/photos/842286/pexels-photo-842286.jpeg?cs=srgb&dl=adult-face-facial-expression-842286.jpg&fm=jpg",
+        "scores": [
+            "3",
+            "3",
+            "3",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3",
+            "4",
+            "2"
+        ]
+    },
+
+    {
+        "name": "Gigi",
+        "photo": "https://images.pexels.com/photos/713312/pexels-photo-713312.jpeg?cs=srgb&dl=beautiful-blur-carefree-713312.jpg&fm=jpg",
+        "scores": [
+            "3",
+            "3",
+            "5",
+            "2",
+            "4",
+            "3",
+            "1",
+            "4",
+            "5",
+            "4"
+        ]
+    },
+
+    {
+        "name": "Henry",
+        "photo": "https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?cs=srgb&dl=adult-beach-beard-736716.jpg&fm=jpg",
+        "scores": [
+            "3",
+            "2",
+            "4",
+            "2",
+            "5",
+            "4",
+            "1",
+            "1",
+            "4",
+            "4"
+        ]
+    },
+
+    {
+        "name": "Isabell",
+        "photo": "https://images.pexels.com/photos/1191488/pexels-photo-1191488.jpeg?cs=srgb&dl=adult-afro-beautiful-1191488.jpg&fm=jpg",
+        "scores": [
+            "2",
+            "4",
+            "4",
+            "4",
+            "4",
+            "3",
+            "4",
+            "3",
+            "4",
+            "5"
+        ]
+    },
+
+    {
+        "name": "Jeneanne",
+        "photo": "https://images.pexels.com/photos/371168/pexels-photo-371168.jpeg?cs=srgb&dl=adult-attractive-beautiful-371168.jpg&fm=jpg",
+        "scores": [
+            "5",
+            "4",
+            "1",
+            "1",
+            "2",
+            "3",
+            "4",
+            "4",
+            "2",
+            "2"
+        ]
+    },
+
+    {
+        "name": "Kibble",
+        "photo": "https://images.pexels.com/photos/356378/pexels-photo-356378.jpeg?cs=srgb&dl=adorable-animal-breed-356378.jpg&fm=jpg",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    }
+]
+
+module.exports = clientList;
